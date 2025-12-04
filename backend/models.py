8fc341b28430d9ca7f12c8f7f6cd7efc707d2426
Krutyash <kostrov2525@gmail.com>
2025-12-04 18:05:35 +0500

    changed default value created_at

--- backend/models.py
+++ backend/models.py
@@ -8,7 +8,7 @@ class Task(db.Model):
     title = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text, nullable=True)
     is_completed = db.Column(db.Boolean, default=False)
-    created_at = db.Column(db.DateTime, default=datetime)
+    created_at = db.Column(db.DateTime, default=datetime.now())
     image_filename = db.Column(db.String(255), nullable=True)
     
     def to_dict(self):
